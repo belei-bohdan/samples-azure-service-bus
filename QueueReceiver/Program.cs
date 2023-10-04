08f3c72215c1bf7a12a6aa7c1366c7e11143fbca
Bogdan Beley <bogdan.beley@outlook.com>
2023-10-04 14:39:26 +0300

    Receiving Messages using Azure Service Bus

--- QueueReceiver/Program.cs
+++ QueueReceiver/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shared.Configurations;
+
+IHostBuilder builder = ConfigureBuilder();
+var host = builder.Build();
+
+using (var cts = new CancellationTokenSource())
+{
+    var cancelTask = Task.Run(() =>
+    {
+        Console.WriteLine("Press any key to cancel...");
+        Console.ReadKey(intercept: true);
+        cts.Cancel();
+    });
+
+    await host.RunAsync(cts.Token);
+    await cancelTask;
+}
+
+static IHostBuilder ConfigureBuilder()
+{
+    return new HostBuilder()
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            config.AddJsonFile("appsettings.json", optional: false);
+
+            if (hostingContext.HostingEnvironment.IsDevelopment())
+            {
+                config.AddJsonFile("appsettings.Development.json", optional: true);
+            }
+
+            config.AddUserSecrets<Program>();
+        })
+        
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddOptions<QueueOptions>()
+            .Bind(hostContext.Configuration.GetSection(QueueOptions.SectionName))
+            .ValidateOnStart();
+            
+            services.AddHostedService<QueueProcessor>();
+        });
+}
---
+++ QueueReceiver/QueueProcessor.cs
@@ -0,0 +1,75 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Shared.Configurations;
+using Shared.Models;
+using System.Text.Json;
+
+internal class QueueProcessor : BackgroundService
+{
+    private readonly QueueOptions _options;
+
+    public QueueProcessor(IOptions<QueueOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await using var client = new ServiceBusClient(_options.Connection);
+
+        var options = new ServiceBusProcessorOptions
+        {
+            AutoCompleteMessages = false,
+            MaxConcurrentCalls = 1,
+        };
+
+        await using var processor = client.CreateProcessor(_options.QueueName);
+
+        processor.ProcessMessageAsync += MessageHandler;
+        processor.ProcessErrorAsync += ErrorHandler;
+
+        await processor.StartProcessingAsync(stoppingToken);
+
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(3000, stoppingToken);
+                Console.WriteLine("Listening...");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Listening cancelled.");
+            await processor.CloseAsync(stoppingToken);
+        }
+    }
+
+    async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string body = args.Message.Body.ToString();
+        var message = JsonSerializer.Deserialize<Message>(body);
+
+        if (message is not null)
+        {
+            DisplayMessage(message);
+            await args.CompleteMessageAsync(args.Message);
+        }
+    }
+
+    private static void DisplayMessage(Message message)
+    {
+        Console.WriteLine($"[{DateTime.Now.TimeOfDay:hh\\:mm\\:ss}] {message.Author}: {message.Content}");
+    }
+
+    Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.ErrorSource);
+        Console.WriteLine(args.FullyQualifiedNamespace);
+        Console.WriteLine(args.EntityPath);
+        Console.WriteLine(args.Exception.Message);
+
+        return Task.CompletedTask;
+    }
+}
